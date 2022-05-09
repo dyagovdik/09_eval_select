58a88e7e142998c57122b33f22e1e0c519c81495
densinc <dvyagovdik@gmail.com>
2022-05-09 04:22:40 +0300

    Extract pipeline creation to a separate module

--- src/RSS_2022_09_forest_ml/train.py
+++ src/RSS_2022_09_forest_ml/train.py
@@ -2,11 +2,10 @@ from pathlib import Path
 
 import click
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
+
+from .pipeline import create_pipeline
 
 @click.command()
 @click.option(
@@ -27,9 +26,7 @@ def train(dataset_path: Path, random_state: int, test_split_ratio: float) -> Non
     features = dataset.drop("target", axis=1)
     target = dataset["target"]
     features_train, features_val, target_train, target_val = train_test_split(features, target, test_size=test_split_ratio, random_state=random_state)
-    scaler = StandardScaler()
-    classifier = LogisticRegression(random_state=random_state)
-    pipeline = Pipeline(steps=[("scaler", scaler), ("classifier", classifier)])
+    pipeline = create_pipeline(random_state)
     pipeline.fit(features_train, target_train)
     accuracy = accuracy_score(target_val, pipeline.predict(features_val))
     click.echo(f"Accuracy: {accuracy}.")
