11410718866832dc2c782b6d5ca757f03aa23a82
densinc <dvyagovdik@gmail.com>
2022-05-09 03:56:30 +0300

    Add random state CLI option

--- src/RSS_2022_09_forest_ml/train.py
+++ src/RSS_2022_09_forest_ml/train.py
@@ -14,14 +14,13 @@ from sklearn.model_selection import train_test_split
     default="data/train.csv",
     type=click.Path(exists=True, dir_okay=False, path_type=Path),
 )
-def train(dataset_path: Path) -> None:
+@click.option("--random-state", default=42, type=int)
+def train(dataset_path: Path, random_state: int) -> None:
     dataset = pd.read_csv(dataset_path)
     click.echo(f"Dataset shape: {dataset.shape}.")
     features = dataset.drop("target", axis=1)
     target = dataset["target"]
-    features_train, features_val, target_train, target_val = train_test_split(
-        features, target, test_size=0.2, random_state=42
-    )
-    classifier = LogisticRegression(random_state=42).fit(features_train, target_train)
+    features_train, features_val, target_train, target_val = train_test_split(features, target, test_size=0.2, random_state=random_state)
+    classifier = LogisticRegression(random_state=random_state).fit(features_train, target_train)
     accuracy = accuracy_score(target_val, classifier.predict(features_val))
     click.echo(f"Accuracy: {accuracy}.")
