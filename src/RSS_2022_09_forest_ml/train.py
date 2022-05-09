77632eb05e58ee6ce36dfd7a212a7716b160e105
densinc <dvyagovdik@gmail.com>
2022-05-09 04:02:47 +0300

    Add test split ratio CLI option

--- src/RSS_2022_09_forest_ml/train.py
+++ src/RSS_2022_09_forest_ml/train.py
@@ -15,12 +15,17 @@ from sklearn.model_selection import train_test_split
     type=click.Path(exists=True, dir_okay=False, path_type=Path),
 )
 @click.option("--random-state", default=42, type=int)
-def train(dataset_path: Path, random_state: int) -> None:
+@click.option(
+    "--test-split-ratio",
+    default=0.2,
+    type=click.FloatRange(0, 1, min_open=True, max_open=True),
+)
+def train(dataset_path: Path, random_state: int, test_split_ratio: float) -> None:
     dataset = pd.read_csv(dataset_path)
     click.echo(f"Dataset shape: {dataset.shape}.")
     features = dataset.drop("target", axis=1)
     target = dataset["target"]
-    features_train, features_val, target_train, target_val = train_test_split(features, target, test_size=0.2, random_state=random_state)
+    features_train, features_val, target_train, target_val = train_test_split(features, target, test_size=test_split_ratio, random_state=random_state)
     classifier = LogisticRegression(random_state=random_state).fit(features_train, target_train)
     accuracy = accuracy_score(target_val, classifier.predict(features_val))
     click.echo(f"Accuracy: {accuracy}.")
