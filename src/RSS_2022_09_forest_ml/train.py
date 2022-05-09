660664efc17a9183733040ba25e31d17a316a312
densinc <dvyagovdik@gmail.com>
2022-05-09 03:00:08 +0300

    Train logistic regression

--- src/RSS_2022_09_forest_ml/train.py
+++ src/RSS_2022_09_forest_ml/train.py
@@ -2,6 +2,9 @@ from pathlib import Path
 
 import click
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
 
 @click.command()
@@ -14,3 +17,11 @@ import pandas as pd
 def train(dataset_path: Path) -> None:
     dataset = pd.read_csv(dataset_path)
     click.echo(f"Dataset shape: {dataset.shape}.")
+    features = dataset.drop("target", axis=1)
+    target = dataset["target"]
+    features_train, features_val, target_train, target_val = train_test_split(
+        features, target, test_size=0.2, random_state=42
+    )
+    classifier = LogisticRegression(random_state=42).fit(features_train, target_train)
+    accuracy = accuracy_score(target_val, classifier.predict(features_val))
+    click.echo(f"Accuracy: {accuracy}.")
