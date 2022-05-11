c61f2d39fd8f944004973b7afafeba6af3c339b8
densinc <dvyagovdik@gmail.com>
2022-05-11 06:50:46 +0300

    Add RFC in pipeline

--- src/09_eval_select/data.py
+++ src/09_eval_select/data.py
@@ -6,11 +6,10 @@ import click
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-
 def get_dataset(
     csv_path: Path, random_state: int, test_split_ratio: float
 ) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
-    dataset = pd.read_csv(csv_path)
+    dataset = pd.read_csv(csv_path).set_index('Id')
     click.echo(f"Dataset shape: {dataset.shape}.")
     features = dataset.drop("Cover_Type", axis=1)
     target = dataset["Cover_Type"]
--- src/09_eval_select/pipeline.py
+++ src/09_eval_select/pipeline.py
@@ -1,10 +1,10 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
 
-
-def create_pipeline(
-    use_scaler: bool, max_iter: int, logreg_C: float, random_state: int
+def create_pipeline_LogReg(
+    use_scaler: bool, max_iter: int, logregc: float, random_state: int
 ) -> Pipeline:
     pipeline_steps = []
     if use_scaler:
@@ -13,7 +13,24 @@ def create_pipeline(
         (
             "classifier",
             LogisticRegression(
-                random_state=random_state, max_iter=max_iter, C=logreg_C
+                random_state=random_state, max_iter=max_iter, C=logregc
+            ),
+        )
+    )
+    return Pipeline(steps=pipeline_steps)
+
+def create_pipeline_RFC(
+    use_scaler: bool, random_state: int, criterion: str, max_depth: int, bootstrap: bool, n_estimators: int 
+) -> Pipeline:
+    pipeline_steps = []
+    if use_scaler:
+        pipeline_steps.append(("scaler", StandardScaler()))
+
+        pipeline_steps.append(
+            (
+                "classifier", 
+                RandomForestClassifier(
+                    n_estimators=n_estimators, random_state=random_state, criterion=criterion, max_depth=max_depth, bootstrap=bootstrap
             ),
         )
     )
